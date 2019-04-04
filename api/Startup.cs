db7271657968e6ed65ddf5ec722db7f62a92ffbb
Alberto Meneghini <menalb84@gmail.com>
2019-04-04 20:22:26 +0200

    restored https

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace header_navigation
@@ -28,24 +21,24 @@ namespace header_navigation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
- {
-     options.AddPolicy(MyAllowSpecificOrigins,
-     builder =>
-     {
-         builder.WithOrigins("http://localhost:4200")
-                             .AllowAnyHeader()
-                             .AllowAnyMethod()
-                             .WithExposedHeaders("Link");
-     });
- });
-
+            {
+                options.AddPolicy(
+                    MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .WithExposedHeaders("Link");
+                    });
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSwaggerGen(c =>
-   {
-       c.SwaggerDoc("v1", new Info { Title = "Products API", Version = "v1" });
-   });
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Products API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +50,7 @@ namespace header_navigation
             }
             else
             {
-                // app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseCors(MyAllowSpecificOrigins);
@@ -71,7 +64,7 @@ namespace header_navigation
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Products API");
             });
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
