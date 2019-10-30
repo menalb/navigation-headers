9504a272dffca7e51854bf8b916541b2e7eaf3bd
alberto.meneghini <alberto.meneghini@retailitaliasrl.it>
2019-10-30 13:42:35 +0100

    update

--- nav-headers/src/app/simple-product/simple-product-box.component.ts
+++ nav-headers/src/app/simple-product/simple-product-box.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { SimpleProductService } from './simple-product.service';
 import { Product } from '../product.model';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-product-box',
@@ -10,13 +11,32 @@ import { Product } from '../product.model';
 export class SimpleProductBoxComponent implements OnInit {
 
   @Input() product: Product;
-
-  constructor(private productService: SimpleProductService) {
-
+  isEditMode = false;
+  productForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private productService: SimpleProductService) {
   }
 
   ngOnInit() {
-
+    this.buildForm();
   }
 
+  buildForm() {
+    this.productForm = this.formBuilder.group({
+      name: new FormControl(this.product.name, Validators.required)
+    });
+  }
+  boxClick(ev) {
+    if (!this.isEditMode) {
+      this.isEditMode = true;
+      this.buildForm();
+    }
+  }
+  save(ev) {
+    this.isEditMode = false;
+    ev.stopPropagation();
+  }
+  cancel(ev) {
+    this.isEditMode = false;
+    ev.stopPropagation();
+  }
 }
